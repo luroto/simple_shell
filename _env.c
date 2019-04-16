97aed540b88ad429d362a0041c6cba8ed0c3c2c8
luroto <47120090+luroto@users.noreply.github.com>
2019-04-15 23:48:40 -0500

    Update _env.c

--- _env.c
+++ _env.c
@@ -1,9 +1,20 @@
 #include "shell.h"
-int _env(void)
+/**
+ *_env - print the environ variables
+ *
+ *@cont: the numers of arguments received for the getline
+ *
+ *Return: if the function fails: -1, else 0;
+ */
+int _env(int cont)
 {
-	extern char **environ;
-	int i;
+	int i = 0;
 
+	if (cont > 1)
+	{
+		perror("Error");
+		return (-1);
+	}
 	for (i = 0; environ[i] != NULL; i++)
 	{
 		write(1, environ[i], _strlen_recursion(environ[i]));
