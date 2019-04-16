5ec12e7520e2c7f69730925e27b62d0416b8445f
xica369 <761@holbertonschool.com>
2019-04-16 04:25:55 +0000

    [feature] function that change a number (int) to a string

---
+++ _convert_num.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+/**
+ *_convert_num - prints int numbers
+ *
+ *@k: number to print
+ *
+ *Return: the number of bytes
+ */
+char *_convert_num(int k)
+{
+	int a = k, b, c = 0, d = 0, e = 1, f = 0, m = 0, n = 0, l = 0;
+	char *x;
+
+	f = a;
+	d = a;
+	for (b = 0; a != 0; b++)
+	{a = a / 10;
+		if (c == 1)
+			e = e * 10;
+		c = 1;
+	}
+	c = b;
+	x = malloc(b + 1);
+	if (x == NULL)
+	{free(x);
+		return (NULL);
+	}
+	for (; b > 0; b--, l++)
+	{       m = d / e;
+		n = m % 10;
+		e = e / 10;
+		if (m < 10)
+		{       x[l] = m + '0';
+		}
+		else
+		{x[l] = n + '0';
+		}
+	}
+	if (f == 0)
+		x[0] = '0';
+	x[l + 1] = '\0';
+	return (x);
+}
